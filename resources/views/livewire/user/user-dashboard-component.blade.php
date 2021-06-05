263a7cfdaac806dcaec5ed3433cc114a4eef520e
muliadigit <adhymaros33@gmail.com>
2021-06-05 12:30:59 +0800

    perubahan pertama

---
+++ resources/views/livewire/user/user-dashboard-component.blade.php
@@ -0,0 +1,11 @@
+
+    <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+          <h1 class="text-center">Welcome, Toko LapakShop</h1>
+            <br/>
+            <p class="text-center fw-bold"> WEB Developer By:</p>
+            <p class="text-center fw-bold"> Muliadi-20192115009</p>
+          </div>
+        </div>
+    </div>
